a420821db6666d4b50474ef1cd93bb40cc1dd8fd
YOGEESHKP555 <yogeesh793@gmail.com>
2024-06-29 16:16:12 +0530

    Add files via upload

---
+++ creating_variables.py
@@ -0,0 +1,110 @@
+# integer
+a=2
+print(a)
+
+#integer
+b=3265626546
+print(b)
+
+# floting value
+c=14.3
+print(c)
+
+#String
+d='a'
+print(d)
+
+#string
+name='veraj'
+print(name)
+
+#boolen
+q=True
+print(q)
+
+#empty value or null data type
+x = None
+print(x)
+
+#sytax error
+#0 = x
+
+# keyword
+import keyword
+print(keyword.kwlist)
+
+#vareable naming
+x=True
+print(x)
+
+_y=True
+print(_y)
+
+# 9x = False
+# print(9x)                  #out put is sytax error because no not define
+
+#$x = False
+#print($x)                   #out put is sytax error because symbol not define
+
+has_init = 'still'
+print(has_init)
+
+# Names are case sensitive
+#x=9
+#j=X*5                      # x is big letter
+#print(j)                   # name error
+
+# data type
+a=1
+print(type(a))
+
+b=23564
+print(type(b))
+
+pi = 14.3
+print(type(pi))
+
+s='f'
+print(type(s))
+
+g=True
+print(type(g))
+
+h=None
+print(type(h))
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+#a,b,c = 1,2
+#print(a,b,c)                    # value error need more 2 value
+
+#a,b = 1,2,3
+#print(a,b)                        # value error need more 2 value
+
+a,b,_ = 1,2,3
+print(a,b)                         # out put 2
+
+#a,b,_ = 1,2,3,4
+#print(a,b)                          # value error need more 2 value
+
+a=b=c=1
+print(a,b,c)                       # output is 1 1 1
+
+b=2
+print(a,b,c)                         # output 121
+
+x=y=[1,2,3]
+x=[5,6,7]
+print(y)
+
+x=y=[1,2,3]
+x[0]=15
+print(y)                                # out put [15,2,3]
+
+u=[1,2,[3,4,5],6,7]
+print(u[2])
+print(u[2][1])                            # second in first value
+
+
+
